90370868c6321f4d29740222ab61c79b9542dec5
HEOHEOHEOseoul <zxz4790@gmail.com>
2022-07-16 20:15:08 +0900

    dictionary update

--- src/main.go
+++ src/main.go
@@ -8,6 +8,12 @@ import (
 func main() {
 	dictionary := mydict.Dictionary{}
 	dictionary.Add("hi", "안녕")
-	fmt.Println(dictionary.Search("hi"))
+	fmt.Println(dictionary)
+	errr := dictionary.Update("hii", "인사")
 
+	if errr == nil {
+		fmt.Println(dictionary)
+	} else {
+		fmt.Println(errr)
+	}
 }
--- src/mydict/mydict.go
+++ src/mydict/mydict.go
@@ -7,8 +7,10 @@ import (
 //Dictionary type
 type Dictionary map[string]string
 
-var errorNotFount = errors.New("not found")
-var errorAlreadyHas = errors.New("this word is already has")
+var (
+	errorNotFount   = errors.New("not found")
+	errorAlreadyHas = errors.New("this word is already has")
+)
 
 //search for a word
 func (d Dictionary) Search(word string) (string, error) {
@@ -19,13 +21,34 @@ func (d Dictionary) Search(word string) (string, error) {
 	return "", errorNotFount
 }
 
+//Add a word to the dictionary
 func (d Dictionary) Add(word, des string) error {
 	_, err := d.Search(word)
-	if err == errorNotFount {
+
+	switch err {
+	case errorNotFount:
 		d[word] = des
-		return nil
-	} else {
+	case nil:
 		return errorAlreadyHas
 	}
+	return nil
 
+}
+
+//Update a description of a word
+func (d Dictionary) Update(word, des string) error {
+	_, err := d.Search(word)
+
+	// if err == errorNotFount {
+	// 	return err
+	// }
+	// d[word] = des
+	// return nil
+	switch err {
+	case errorNotFount:
+		return errorNotFount
+	case nil:
+		d[word] = des
+	}
+	return nil
 }
